2ecb3c23db52b347c2f0aebd7e912499e6fe30c0
MBV <malkov.b@gmail.com>
2015-12-17 10:46:47 +0200

    Мой первый бекап

---
+++ CustomerViewer/build/classes-generated/org/shop/viewer/Bundle.java
@@ -0,0 +1,48 @@
+package org.shop.viewer;
+/** Localizable strings for {@link org.shop.viewer}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>CustomerEditor</i>
+     * @see CustomerEditorTopComponent
+     */
+    static String CTL_CustomerEditorAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CustomerEditorAction");
+    }
+    /**
+     * @return <i>CustomerEditor Window</i>
+     * @see CustomerEditorTopComponent
+     */
+    static String CTL_CustomerEditorTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CustomerEditorTopComponent");
+    }
+    /**
+     * @return <i>CustomerViewer</i>
+     * @see CustomerViewerTopComponent
+     */
+    static String CTL_CustomerViewerAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CustomerViewerAction");
+    }
+    /**
+     * @return <i>CustomerViewer Window</i>
+     * @see CustomerViewerTopComponent
+     */
+    static String CTL_CustomerViewerTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_CustomerViewerTopComponent");
+    }
+    /**
+     * @return <i>This is a CustomerEditor window</i>
+     * @see CustomerEditorTopComponent
+     */
+    static String HINT_CustomerEditorTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_CustomerEditorTopComponent");
+    }
+    /**
+     * @return <i>This is a CustomerViewer window</i>
+     * @see CustomerViewerTopComponent
+     */
+    static String HINT_CustomerViewerTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_CustomerViewerTopComponent");
+    }
+    private void Bundle() {}
+}
